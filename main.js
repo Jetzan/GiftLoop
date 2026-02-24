756e4f69bbf232bff7f59266220ed7d6f7e03435
Jetzan Esquivel <jetzan434@gmail.com>
2026-02-23 21:53:29 -0600

    feat: inicializa proyecto y agrega panel para nombre del organizador

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+const buttonNuevoSorteo =document.getElementById("nuevoSorteo");
+const sectionPanel = document.querySelector(".panel")
+
+buttonNuevoSorteo.addEventListener("click",()=>{
+    sectionPanel.style.display="flex";
+})
